b72d23c877d5e18d198f46b5ef5ba15cc3ce50c3
AinSid <a.siddiqui0014@gmail.com>
2025-01-26 03:40:28 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/workout-tracker/',
   server: {
-    port: 3000,
-    open: true
-  }
+    port: 3002,
+    host: true,
+    historyApiFallback: true
+  },
+  base: '/'
 }) 
